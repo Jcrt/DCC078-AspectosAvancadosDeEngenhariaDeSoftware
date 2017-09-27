757939d85dc4b18eb39447c950040ce5651fb017
Jcrt <julio.rosatrindade@gmail.com>
2017-09-27 01:50:22 -0300

    Ajuste de jstl e submit de alteração de estoque

--- ObserverPattern/src/java/Actions/AlterarEstoqueProdutoAction.java
+++ ObserverPattern/src/java/Actions/AlterarEstoqueProdutoAction.java
@@ -11,6 +11,10 @@ import Models.Contato;
 import Models.Produto;
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,7 +26,7 @@ public class AlterarEstoqueProdutoAction implements Action {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
+        Produto p;
         try {
             int idProduto = Integer.parseInt((String) request.getParameter("idProduto"));
             int estoque = Integer.parseInt((String) request.getParameter("estoque"));
@@ -30,17 +34,23 @@ public class AlterarEstoqueProdutoAction implements Action {
             ProdutoDAO pDAO = ProdutoDAO.getInstance();
             ContatoDAO cDAO = ContatoDAO.getInstance();
             
-            Produto p = pDAO.ler(idProduto);
+            p = pDAO.ler(idProduto);
             
             List<Contato> c = cDAO.listarObservadorProduto(p);
             
             p.setEstoque(estoque);
+            pDAO.alterarEstoque(p);
             
+            request.setAttribute("MsgOperacao", "Estoque do produto " + p.getNome() + " alterado com sucesso!");
         } catch (Exception e) {
-            
+            request.setAttribute("MsgOperacao", "Erro na alteração do estoque do produto");
         }
         
-        
-        
+        RequestDispatcher rd = request.getRequestDispatcher("FrontController?action=ListarProdutos");
+        try {
+            rd.forward(request, response);
+        } catch (ServletException ex) {
+            Logger.getLogger(AlterarEstoqueProdutoAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
